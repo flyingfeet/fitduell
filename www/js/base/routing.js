72b1a7e5f3f3727cc83552156b40f85f085fda90
Markus Klein <klein.markus90@gmx.de>
2014-12-05 11:34:09 +0100

    FUCK SOURCETREE 3.

--- www/js/base/routing.js
+++ www/js/base/routing.js
@@ -148,5 +148,5 @@ angular.module('challenger')
         }
       });
 // if none of the above states are matched, use this as the fallback
-    $urlRouterProvider.otherwise('/app/timeline');
+    $urlRouterProvider.otherwise('/app/timeline/list');
   });
