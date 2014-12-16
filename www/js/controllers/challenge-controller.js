bf3c603e4fbc4c4c698cf828e138527bc1843c33
Markus Klein <klein.markus90@gmx.de>
2014-12-16 15:03:41 +0100

    ghh

--- www/js/controllers/challenge-controller.js
+++ www/js/controllers/challenge-controller.js
@@ -86,35 +86,35 @@ angular.module('challenger')
       });
     };
 
-    //var cameraOptions = {
-    //  quality: 100,
-    //  destinationType: Camera.DestinationType.DATA_URL,
-    //  sourceType: Camera.PictureSourceType.CAMERA,
-    //  allowEdit: true,
-    //  encodingType: Camera.EncodingType.JPEG,
-    //  targetWidth: 300,
-    //  targetHeight: 300,
-    //  popoverOptions: CameraPopoverOptions,
-    //  saveToPhotoAlbum: false
-    //};
-    //
-    //$scope.getPicture = function () {
-    //  $cordovaCamera.getPicture(cameraOptions).then(function (imageData) {
-    //    var promise = ChallengesService.uploadProofImage(id, imageData);
-    //    promise.then(function (challenge) {
-    //      $scope.selectedChallenge = challenge;
-    //      for (var i = 0; i < $scope.timelineChallenges.length; i++) {
-    //        if ($scope.timelineChallenges[i].id === $scope.selectedChallenge.id) {
-    //          $scope.timelineChallenges[i] = challenge;
-    //        }
-    //      }
-    //
-    //      $cordovaToast.showShortBottom("Bild erfolgreich hochgeladen.");
-    //    })
-    //  }, function (err) {
-    //    $cordovaToast.showLongBottom(err);
-    //  });
-    //};
+    var cameraOptions = {
+      quality: 100,
+      destinationType: Camera.DestinationType.DATA_URL,
+      sourceType: Camera.PictureSourceType.CAMERA,
+      allowEdit: true,
+      encodingType: Camera.EncodingType.JPEG,
+      targetWidth: 300,
+      targetHeight: 300,
+      popoverOptions: CameraPopoverOptions,
+      saveToPhotoAlbum: false
+    };
+
+    $scope.getPicture = function () {
+      $cordovaCamera.getPicture(cameraOptions).then(function (imageData) {
+        var promise = ChallengesService.uploadProofImage(id, imageData);
+        promise.then(function (challenge) {
+          $scope.selectedChallenge = challenge;
+          for (var i = 0; i < $scope.timelineChallenges.length; i++) {
+            if ($scope.timelineChallenges[i].id === $scope.selectedChallenge.id) {
+              $scope.timelineChallenges[i] = challenge;
+            }
+          }
+
+          $cordovaToast.showShortBottom("Bild erfolgreich hochgeladen.");
+        })
+      }, function (err) {
+        $cordovaToast.showLongBottom(err);
+      });
+    };
 
     $scope.updateStatus = function (status, winner) {
       var promise = ChallengesService.updateStatus(id, status, winner);
